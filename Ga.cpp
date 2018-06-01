69209c3aca2b7a1106de59bd648102cac6189c0f
Bennett Gale <bennett@Bennetts-MBP.lan>
2018-06-01 17:13:29 +1000

    n=100 75000, smaller pop

--- Ga.cpp
+++ Ga.cpp
@@ -25,21 +25,17 @@ void Ga::crossover_all()
 {
 	for (int i = pool_size - 1; i > pop_size; i--)
 	{
-		State_ptr parent1 = population[rand() % pop_size];
-		State_ptr parent2 = population[rand() % pop_size];
+		State_ptr parent1 = population[rand() % 6];
+		State_ptr parent2 = population[rand() % 6];
 		population[i]->absorb(*parent1, *parent2);
 	}
 }
 
 void Ga::mutate_all()
 {
-	// for (int i = 0; i < pool_size; i++)
-	// 	population[i]->mutate(10);
 	for (int i = 0; i < pop_size; i++)
 	{
-		int j = i + pop_size;
-		population[j] = population[i];
-		population[j]->mutate(100);
+		population[i]->mutate((i + 1) * 5);
 	}
 }
 
@@ -53,15 +49,15 @@ int Ga::run()
 	int generation;
 	for(generation = 0; generation < max_generations; generation++)
 	{
-		// if (generation % 100 == 0 && generation > 1)
-		// {
-		// 	std::cout << "Generation: " << generation << std::endl;
-		// 	world_best->print();
-		// }
+		if (generation % 10000 == 0 && generation > 1)
+		{
+			std::cout << "Generation: " << generation << std::endl;
+			world_best->print();
+		}
 		crossover_all();
 		mutate_all();
 		std::sort(population.begin(), population.end(), comp);
-		population[pool_size - 1]->randomize();
+		population[pop_size - 1]->randomize();
 		world_best = population[0];
 		if (world_best->fitness.overall == 0)
 			break;
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
 {
 	srand(time(NULL));
 	/* int n, int pop_size, int max_generations, int segment_divisor */
-	Ga solver(100, 1000, 1000000, 0);
+	Ga solver(100, 12, 1000000, 0);
 	int generations = solver.run();
 	std::cout << "Solution found in " << generations << " iterations" << std::endl;
 	return 0;
